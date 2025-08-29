f654e091352167fd53a343d83d3d2d85be112cb2
LokeDev <o.carmonadb@gmail.com>
2025-08-29 11:50:35 +0200

    BaseAttributeSet Created on C++

---
+++ Plaground/Source/Plaground/GAS/AttributeSets/BaseAttributeSet.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseAttributeSet.h"
+
+UBaseAttributeSet::UBaseAttributeSet() : Health(100), MaxHealth(100), Damage(10),
+Armor(0),
+MaxSpeed(600), Acceleration(2048), Deacceleration(2048), TurnRateZ(360)
+{
+}
